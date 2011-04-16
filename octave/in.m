0327acdb55e2e7f8f0abdd1e441d4f4270d5ab33
Drew Wagner <drewm1980@gmail.com>
2011-04-15 21:48:03 -0500

    Added stuff for GNU Octave

---
+++ octave/in.m
@@ -0,0 +1,20 @@
+function data = in(filename="", stride=1, type="float32")
+fd = fopen(filename);
+data = fread(fd, Inf, type);
+fclose(fd);
+if mod(length(data),stride),
+	disp("Array length not multiple of stride; some likely options are:");
+	p = factor(length(data));
+	for i = 1:length(p)
+		term1 = prod(p(1:i));
+		term2 = prod(p(i+1:end));
+		disp([num2str(term1) " x " num2str(term2)]);
+	end
+	exit
+end
+data = reshape(data, stride, length(data)/stride);
+data = data';
+if nargout==0,
+	[dir, varname, ext, ver] = fileparts(filename);
+	assignin("caller", varname, data);
+end
