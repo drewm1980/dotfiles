1092c86b4d5b49b39420fd7534562bc51171398d
Andrew Wagner <andrew.wagner@esat.kuleuven.be>
2013-06-13 14:17:10 +0200

    Updating plugins and vim setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ echo ". ~/dotfiles/aliases" >> ~/.bashrc
 ln -sf ~/dotfiles/inputrc            ~/.inputrc
 ln -sf ~/dotfiles/gitconfig          ~/.gitconfig
 ln -sf ~/dotfiles/vim/vimrc          ~/.vimrc
+rm -rf ~/.vim
 ln -sf ~/dotfiles/vim                ~/.vim
 ln -sf ~/dotfiles/awesome            ~/.config/awesome
 ln -sf ~/dotfiles/i3            ~/.i3
